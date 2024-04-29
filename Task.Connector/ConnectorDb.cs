6be20de728fbe231177fcd005c26f7f459a140e1
Kurdanov Feytullah <fkurdan@gmail.com>
2024-04-29 21:51:22 +0300

    Implement RemoveUserPermissions and GetUserPermissions methods and update CreateUser

--- Task.Connector/ConnectorDb.cs
+++ Task.Connector/ConnectorDb.cs
@@ -4,13 +4,13 @@ using Task.Integration.Data.Models.Models;
 
 namespace Task.Connector
 {
+    // В тестах нужно будет изменить строку подключения для MSSQL, а также аргументы в FileLogger на подобный формат: $"{DateTime.Now.ToString("MM.dd.yyyy HH-mm-ss")}connector{provider}.Log", чтобы не вылетала ошибка
     public class ConnectorDb : IConnector
     {
-        private string _connectionString;
-        public ILogger Logger { get; set; }
-
         public ConnectorDb() { }
 
+        private string _connectionString;
+        public ILogger Logger { get; set; }
 
         public void StartUp(string connectionString)
         {
@@ -19,22 +19,40 @@ namespace Task.Connector
 
         public void CreateUser(UserToCreate user)
         {
-            // Создание нового пользователя в базе данных с набором свойств по умолчанию
+            // Создание нового пользователя 
             using (var connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
 
-                var query = $"INSERT INTO [TestTaskSchema].[User] " +
-                    $"(Login, {string.Join(", ", user.Properties.Select(p => p.Name))}) " +
-                    $"VALUES (@Login, {string.Join(", ", user.Properties.Select(p => '@' + p.Name))})";
+                var query = $"INSERT INTO [TestTaskSchema].[User]" +
+                    $"(Login, " +
+                    $"LastName, " +
+                    $"FirstName, " +
+                    $"MiddleName, " +
+                    $"TelephoneNumber, " +
+                    $"IsLead) " +
+                    "VALUES " +
+                    "(@Login, " +
+                    "@LastName, " +
+                    "@FirstName, " +
+                    "@MiddleName, " +
+                    "@TelephoneNumber, " +
+                    "@IsLead)";
 
                 using (var command = new SqlCommand(query, connection))
                 {
+                    var lastName = user.Properties.FirstOrDefault(p => p.Name == "lastName")?.Value;
+                    var firstName = user.Properties.FirstOrDefault(p => p.Name == "firstName")?.Value;
+                    var middleName = user.Properties.FirstOrDefault(p => p.Name == "middleName")?.Value;
+                    var telephoneNumber = user.Properties.FirstOrDefault(p => p.Name == "telephoneNumber")?.Value;
+                    var isLead = user.Properties.FirstOrDefault(p => p.Name == "isLead")?.Value;
+
                     command.Parameters.AddWithValue("@Login", user.Login);
-                    foreach (var property in user.Properties)
-                    {
-                        command.Parameters.AddWithValue($"@{property.Name}", property.Value);
-                    }
+                    command.Parameters.AddWithValue("@LastName", lastName ?? "user");
+                    command.Parameters.AddWithValue("@FirstName", firstName ?? "firstName");
+                    command.Parameters.AddWithValue("@MiddleName", middleName ?? "middleName");
+                    command.Parameters.AddWithValue("@TelephoneNumber", telephoneNumber?? "8000000000");
+                    command.Parameters.AddWithValue("@IsLead", isLead ?? "0");
 
                     int result = command.ExecuteNonQuery();
                 }
@@ -45,11 +63,14 @@ namespace Task.Connector
 
         public bool IsUserExists(string userLogin)
         {
+            // Проверка существования пользователя
             using (var connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
 
-                var query = "SELECT COUNT(*) FROM [TestTaskSchema].[User] WHERE Login = @Login";
+                var query = "SELECT COUNT(*) " +
+                    "FROM [TestTaskSchema].[User] " +
+                    "WHERE Login = @Login";
 
                 using (var command = new SqlCommand(query, connection))
                 {
@@ -64,6 +85,7 @@ namespace Task.Connector
 
         public IEnumerable<Property> GetAllProperties()
         {
+            // Получение всех свойств
             var properties = new List<Property>();
 
             using (var connection = new SqlConnection(_connectionString))
@@ -92,6 +114,7 @@ namespace Task.Connector
 
         public IEnumerable<UserProperty> GetUserProperties(string userLogin)
         {
+            // Получение свойств пользователя в системе
             var userProperties = new List<UserProperty>();
 
             using (var connection = new SqlConnection(_connectionString))
@@ -129,6 +152,7 @@ namespace Task.Connector
 
         public void UpdateUserProperties(IEnumerable<UserProperty> properties, string userLogin)
         {
+            // Обновление свойств пользователя в системе
             using (var connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
@@ -156,6 +180,7 @@ namespace Task.Connector
 
         public IEnumerable<Permission> GetAllPermissions()
         {
+            // Получение всех прав пользователя в системе
             var permissions = new List<Permission>();
 
             using (var connection = new SqlConnection(_connectionString))
@@ -226,7 +251,7 @@ namespace Task.Connector
                     }
                     else
                     {
-                        throw new ArgumentException($"Invalid right ID format: {rightId}");
+                        Logger.Error($"Invalid right ID format: {rightId}");
                     }
 
                 }
@@ -237,12 +262,87 @@ namespace Task.Connector
 
         public void RemoveUserPermissions(string userLogin, IEnumerable<string> rightIds)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                foreach (var rightId in rightIds)
+                {
+                    if (rightId.StartsWith("Request"))
+                    {
+                        string query = "DELETE FROM " +
+                            "[TestTaskSchema].[UserRequestRight] " +
+                            "WHERE userId = @userId AND rightId = rightId;";
+
+                        using (var command = new SqlCommand(query, connection))
+                        {
+                            command.Parameters.Clear();
+                            command.Parameters.AddWithValue("@userId", userLogin);
+
+                            var requestRightId = rightId.Replace("Request:", "");
+                            command.Parameters.AddWithValue("@rightId", rightId.Replace("Request:", ""));
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                    else if (rightId.StartsWith("Role"))
+                    {
+                        string query = "DELETE FROM " +
+                            "[TestTaskSchema].[UserItRole] " +
+                            "WHERE userId = @userId AND roleId = @roleId;";
+
+                        using (var command = new SqlCommand(query, connection))
+                        {
+                            command.Parameters.Clear();
+                            command.Parameters.AddWithValue("@userId", userLogin);
+
+                            var itRoleId = rightId.Replace("Role:", "");
+                            command.Parameters.AddWithValue("@roleId", rightId.Replace("Role:", ""));
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                    else
+                    {
+                        Logger.Error($"Invalid right ID format: {rightId}");
+                    }
+
+                }
+
+                Logger.Debug($"Permissions removed from user {userLogin} successfully.");
+            }
+
         }
 
         public IEnumerable<string> GetUserPermissions(string userLogin)
         {
-            throw new NotImplementedException();
+            var permissions = new List<string>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var query = "SELECT 'Request' + CAST(rightId AS VARCHAR(10)) " +
+                    "FROM [TestTaskSchema].[UserRequestRight] " +
+                    "WHERE userId = @userId " +
+                    "UNION " +
+                    "SELECT 'Role' + CAST(roleId AS VARCHAR(10)) " +
+                    "FROM [TestTaskSchema].[UserItRole] " +
+                    "WHERE userId = @userId";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@userId", userLogin);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var permissionId = reader.GetString(0);
+                            permissions.Add(permissionId);
+                        }
+                    }
+                }
+            }
+
+            return permissions;
         }
 
     }
