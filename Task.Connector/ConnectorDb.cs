efed4f30fc36bdf072110e7274db89b1b864d518
Kurdanov Feytullah <fkurdan@gmail.com>
2024-04-29 15:49:08 +0300

    Implement CreateUser, IsUserExists, GetAllProperties, GetUserProperties, UpdateUserProperties methods

--- Task.Connector/ConnectorDb.cs
+++ Task.Connector/ConnectorDb.cs
@@ -9,7 +9,7 @@ namespace Task.Connector
         private string _connectionString;
         public ILogger Logger { get; set; }
 
-        public ConnectorDb () { }
+        public ConnectorDb() { }
 
 
         public void StartUp(string connectionString)
@@ -24,17 +24,18 @@ namespace Task.Connector
             {
                 connection.Open();
 
-                var query = "INSERT INTO [User] (Login, LastName, FirstName, MiddleName, TelephoneNumber, IsLead) " +
-                            "VALUES (@Login, @LastName, @FirstName, @MiddleName, @TelephoneNumber, @IsLead)";
+                var query = $"INSERT INTO [TestTaskSchema].[User] (Login, {string.Join(", ", user.Properties.Select(p => p.Name))}) " +
+                $"VALUES (@Login, {string.Join(", ", user.Properties.Select(p => '@' + p.Name))})";
 
                 using (var command = new SqlCommand(query, connection))
                 {
+                    command.Parameters.AddWithValue("@Login", user.Login);
                     foreach (var property in user.Properties)
                     {
                         command.Parameters.AddWithValue($"@{property.Name}", property.Value);
                     }
 
-                    command.ExecuteNonQuery();
+                    int result = command.ExecuteNonQuery();
                 }
 
                 Logger.Debug($"User {user.Login} created");
@@ -47,7 +48,7 @@ namespace Task.Connector
             {
                 connection.Open();
 
-                var query = "SELECT COUNT(*) FROM [User] WHERE Login = @Login";
+                var query = "SELECT COUNT(*) FROM [TestTaskSchema].[User] WHERE Login = @Login";
 
                 using (var command = new SqlCommand(query, connection))
                 {
@@ -62,18 +63,95 @@ namespace Task.Connector
 
         public IEnumerable<Property> GetAllProperties()
         {
-            throw new NotImplementedException();
+            var properties = new List<Property>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var query = "SELECT Name " +
+                    "FROM sys.columns " +
+                    "WHERE object_id = OBJECT_ID('[TestTaskSchema].[User]')";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var propertyName = reader.GetString(0);
+                            properties.Add(new Property(propertyName, propertyName));
+                        }
+                    }
+                }
+            }
+
+            return properties;
         }
 
         public IEnumerable<UserProperty> GetUserProperties(string userLogin)
         {
-            throw new NotImplementedException();
+            var userProperties = new List<UserProperty>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var query = "SELECT " +
+                    "LastName, " +
+                    "FirstName, " +
+                    "MiddleName, " +
+                    "TelephoneNumber, " +
+                    "IsLead " +
+                    "FROM [TestTaskSchema].[User] WHERE Login = @Login";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Login", userLogin);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            userProperties.Add(new UserProperty("LastName", reader.GetString(0)));
+                            userProperties.Add(new UserProperty("FirstName", reader.GetString(1)));
+                            userProperties.Add(new UserProperty("MiddleName", reader.GetString(2)));
+                            userProperties.Add(new UserProperty("TelephoneNumber", reader.GetString(3)));
+                            userProperties.Add(new UserProperty("IsLead", reader.GetBoolean(4).ToString()));
+                        }
+                    }
+                }
+            }
+
+            return userProperties;
         }
 
 
         public void UpdateUserProperties(IEnumerable<UserProperty> properties, string userLogin)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var query = "UPDATE [TestTaskSchema].[User] SET " +
+                            $"{string.Join(", ",
+                                properties.Select(p => $"{p.Name} = @{p.Name} "))} " +
+                            "WHERE Login = @Login";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Login", userLogin);
+
+                    foreach (var property in properties)
+                    {
+                        command.Parameters.AddWithValue($"@{property.Name}", property.Value);
+                    }
+
+                    command.ExecuteNonQuery();
+                }
+
+                Logger.Debug($"User {userLogin} properties updated successfully.");
+            }
         }
 
         public IEnumerable<Permission> GetAllPermissions()
