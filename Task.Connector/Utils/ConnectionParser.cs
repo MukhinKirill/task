d98b2f8e964f552a8222b60bb0c02016901c6702
Пользователь <123rin12320@gmail.com>
2024-05-11 18:31:47 +0300

    реализация интерфейса коннектора

--- Task.Connector/Utils/ConnectionParser.cs
+++ Task.Connector/Utils/ConnectionParser.cs
@@ -13,18 +13,7 @@ namespace Task.Connector.Utils
     {
         public string GetConnectionProvaider(string connectionString)
         {
-            /*var pairs = connectionString.Split(';', StringSplitOptions.RemoveEmptyEntries)
-                             .Select(pair => pair.Split('='))
-                             .ToDictionary(pair => pair[0].Trim(), pair => pair[1].Trim());
-            var pairs = connectionString.Split(';', StringSplitOptions.RemoveEmptyEntries)
-                 .Select(pair => pair.Split('='))
-                 .Where(pair => pair.Length == 2) // Убедимся, что пара содержит и ключ, и значение
-                 .ToDictionary(pair => pair[0].Trim(), pair => pair[1].Trim());
-
-            string provaider = pairs.ContainsKey("Provaider") ? pairs["Provaider"] : "Unknown";
-
-            return provaider;*/
-
+            
             string[] parts = connectionString.Split(';');
             foreach (string part in parts)
             {
@@ -40,26 +29,9 @@ namespace Task.Connector.Utils
                     {
                         return "MSSQL";
                     }
-                    // Добавьте другие провайдеры, если они используются
                 }
             }
             return "Unknown";
-        }
-
-
-        public string GetUsername(string connectionString)
-        {
-            string username;
-            string provaiderName = GetConnectionProvaider(connectionString);
-
-            if (provaiderName == "MSSSQL")
-                username = new SqlConnectionStringBuilder(connectionString).UserID;
-            else if (provaiderName == "POSTGRE")
-                username = new Npgsql.NpgsqlConnectionStringBuilder(connectionString).Username;
-            else
-                throw new Exception("Неизвестный провайдер базы данных!");
-
-            return username;
         }
 
         public string DBConnectionString(string connectionString)
