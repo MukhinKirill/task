dd5a09768a49a6ab9fd8008cd877e09acbfd222b
AlexGubkovich <alex.gubkovich@gmail.com>
2024-06-24 17:41:53 +0300

    Created extension methods for the connection string and for the User. Implemented StartUp and CreateUser methods in ConnectorDb.

--- Task.Connector/ConnectorDb.cs
+++ Task.Connector/ConnectorDb.cs
@@ -1,18 +1,50 @@
 ﻿using Task.Integration.Data.Models;
 using Task.Integration.Data.Models.Models;
+using Task.Integration.Data.DbCommon;
+using Task.Integration.Data.DbCommon.DbModels;
 
 namespace Task.Connector
 {
     public class ConnectorDb : IConnector
     {
+        private DbContextFactory? _contextFactory;
+        private string? _provider;
+
+        public required ILogger Logger { get; set; }
+
         public void StartUp(string connectionString)
         {
-            throw new NotImplementedException();
+            var innnerConnString = connectionString.GetInnerConnectionString();
+            _contextFactory = new DbContextFactory(innnerConnString);
+            _provider = connectionString.GetProviderName();
         }
 
         public void CreateUser(UserToCreate user)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var userForDb = new User {
+                    Login = user.Login
+                };
+
+                var password = new Sequrity
+                {
+                    Password = user.HashPassword,
+                    UserId = user.Login
+                };
+
+                userForDb.SetPropertiesToUser(user.Properties);
+
+                using var context = GetDataContext();
+                context.Users.Add(userForDb);
+                context.Passwords.Add(password);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"An error occurred while creating a user. \nError: {ex}");
+                throw;
+            }
         }
 
         public IEnumerable<Property> GetAllProperties()
@@ -55,6 +87,15 @@ namespace Task.Connector
             throw new NotImplementedException();
         }
 
-        public ILogger Logger { get; set; }
+        private DataContext GetDataContext()
+        {
+            if (_contextFactory == null || _provider == null)
+            {
+                Logger.Error($"Before using {nameof(ConnectorDb)}, you must call the {nameof(StartUp)} method to configure it.");
+                throw new Exception("StartUp method wasn't called.");
+            }
+
+            return _contextFactory.GetContext(_provider);
+        }
     }
 }
---
+++ Task.Connector/UserHelper.cs
@@ -0,0 +1,30 @@
+﻿using Task.Integration.Data.DbCommon.DbModels;
+using Task.Integration.Data.Models.Models;
+
+namespace Task.Connector;
+
+public static class UserHelper
+{
+    public static void SetPropertiesToUser(this User user, IEnumerable<UserProperty> properties)
+    {
+        var isLead = false;
+        var isLeadString = GetPropertyValueOrEmpty(properties, "isLead");
+
+        if (isLeadString != null && !bool.TryParse(isLeadString, out isLead))
+        {
+            throw new ArgumentException("isLead property should be boolean.");
+        } 
+
+        user.FirstName = GetPropertyValueOrEmpty(properties, "firstName");
+        user.LastName = GetPropertyValueOrEmpty(properties, "lastName");
+        user.MiddleName = GetPropertyValueOrEmpty(properties, "middleName");
+        user.TelephoneNumber = GetPropertyValueOrEmpty(properties, "telephoneNumber");
+        user.IsLead = isLead;
+    }
+
+    private static string GetPropertyValueOrEmpty(IEnumerable<UserProperty> properties, string propertyName)
+    {
+        var value = properties.FirstOrDefault(x => x.Name == propertyName)?.Value;
+        return value ?? "";
+    }
+}
