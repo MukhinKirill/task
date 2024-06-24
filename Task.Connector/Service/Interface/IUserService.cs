650f4cdd47cc12694c08ad11f553ce7c59b33c82
Zhemoedov <tema.zhemoedov1999@gmail.com>
2024-06-24 13:34:59 +0300

    connector

---
+++ Task.Connector/Service/Interface/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Connector.Models;
+using Task.Integration.Data.DbCommon.DbModels;
+using Task.Integration.Data.Models.Models;
+
+namespace Task.Connector.Service.Interface
+{
+    public interface IUserService
+    {
+        PropertyModel ParseProperties(IEnumerable<UserProperty> userProperties);
+
+        IEnumerable<UserProperty> SerializeUserPropertyFromUser(User entity);
+    }
+}
