1d997fab28932cc347389cd6bf80dc6421950cc9
917sympathies <maraturazbakhtin@yandex.ru>
2024-07-03 17:54:13 +0400

    services implemented

---
+++ Task.Connector/Services/PermissionService.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task.Connector.Context;
+using Task.Connector.Entities;
+using Task.Connector.Errors;
+using Task.Connector.Interfaces;
+using Task.Integration.Data.Models;
+using Task.Integration.Data.Models.Models;
+
+namespace Task.Connector.Services;
+
+public class PermissionService : IPermissionService
+{
+    private readonly DatabaseContext _context;
+
+    private readonly ILogger _logger;
+    
+    public PermissionService(DatabaseContext context, ILogger logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public IEnumerable<Permission> GetAllPermissions()
+    {
+        var roles = _context!.ItRoles
+            .Select(role =>
+                new Permission(
+                    $"{role.name}-{role.corporatePhoneNumber}",
+                    role.name, 
+                    $"Корпоративный номер телефона - {role.corporatePhoneNumber}"))
+            .AsEnumerable();
+        var rights = _context!.RequestRights
+            .Select(right => new Permission(right.id.ToString(), right.name, string.Empty))
+            .AsEnumerable();
+        return roles.Concat(rights);
+    }
+
+    public void AddUserPermissions(string userLogin, IEnumerable<string> rightIds)
+    {
+        var user =  _context!.Users.FirstOrDefault(user => user.login == userLogin);
+        if (user is null) Error.Throw(_logger, new ArgumentException($"GetUserProperties(userLogin) : Пользователя с логином {userLogin} не существует"));
+            
+        foreach (var rightId in rightIds)
+        {
+            var rightInfo = rightId.Split(':');
+            var type = rightInfo[0];
+            var id = int.Parse(rightInfo[1]);
+            if (type == "Role")
+            {
+                var role = rightId
+                    .Select(_ => new UserITRole() { roleId = id, userId = user!.login })
+                    .FirstOrDefault();
+                if (role is not null) _context!.UserITRoles.Add(role);
+            }
+            else if (type == "Request"){
+                var right = rightId
+                    .Select(_ => new UserRequestRight() {rightId = id, userId = user!.login})
+                    .FirstOrDefault();
+                if (right is not null) _context!.UserRequestRights.Add(right);
+                break;
+            }
+            else throw new ArgumentException("Неверный формат");
+        }
+        _context.SaveChanges();
+    }
+
+    public void RemoveUserPermissions(string userLogin, IEnumerable<string> rightIds)
+    {
+        var user = _context!.Users.FirstOrDefault(user => user.login == userLogin);
+        if (user is null) Error.Throw(_logger, new ArgumentException($"GetUserProperties(userLogin) : Пользователя с логином {userLogin} не существует"));
+            
+        foreach (var rightId in rightIds)
+        {
+            var rightInfo = rightId.Split(':');
+            var type = rightInfo[0];
+            var id = int.Parse(rightInfo[1]);
+            if (type == "Role")
+            {
+                var role = rightId
+                    .Select(_ => new UserITRole() {roleId = id, userId = user!.login})
+                    .FirstOrDefault();
+                if(role is not null) _context!.UserITRoles.Remove(role);
+            }
+            else if (type == "Request")
+            {
+                var right = rightId
+                    .Select(_ => new UserRequestRight() {rightId = id, userId = user!.login})
+                    .FirstOrDefault();
+                if(right is not null) _context!.UserRequestRights.Remove(right);
+            }
+            else throw new ArgumentException("Неверный формат");
+        }
+
+        _context.SaveChanges();
+    }
+
+    public IEnumerable<string> GetPermissionByUserLogin(string userLogin)
+    {
+        var roles = _context!.UserITRoles
+            .Where(uir => uir.userId == userLogin)
+            .Select(uir => _context.ItRoles.First(role => role.id == uir.roleId).name)
+            .AsEnumerable();
+        var rights = _context!.UserRequestRights
+            .Where(urr => urr.userId == userLogin)
+            .Select(urr => _context.RequestRights.First(right => right.id == urr.rightId).name)
+            .AsEnumerable();
+        return roles.Concat(rights);
+    }
+}
---
+++ Task.Connector/Services/UserService.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task.Connector.Context;
+using Task.Connector.Entities;
+using Task.Connector.Errors;
+using Task.Connector.Interfaces;
+using Task.Integration.Data.Models;
+using Task.Integration.Data.Models.Models;
+
+namespace Task.Connector.Services;
+
+public class UserService : IUserInterface
+{
+    private readonly DatabaseContext _context;
+
+    private readonly ILogger _logger;
+    
+    public UserService(DatabaseContext context, ILogger logger)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public void CreateUser(UserToCreate userToCreate)
+    {
+        var user = new User();
+        foreach (var prop in user.GetType().GetProperties())
+        {
+            var property = userToCreate.Properties.FirstOrDefault(p => p.Name == prop.Name); 
+            if ( property is null)
+            {
+                if (prop.PropertyType == typeof(bool)) prop.SetValue(user, false);
+                else prop.SetValue(user, string.Empty);
+            }
+            else
+            {
+                if (prop.PropertyType == typeof(bool))
+                    prop.SetValue(user, property.Value.ToLower().Contains("true"));
+                else prop.SetValue(user, property.Value);
+            }
+        }
+        user.login = userToCreate.Login;
+        var nPassword = new Password() { userId = user.login, password1 = userToCreate.HashPassword };
+        
+        _context.Users.Add(user);
+        _context.Passwords.Add(nPassword);
+        _context.SaveChanges();
+    }
+
+    public User GetUserByLogin(string userLogin)
+    {
+        var user = _context.Users.FirstOrDefault(user => user.login == userLogin);
+        if (user is null) Error.Throw(_logger, new ArgumentException($"GetUserProperties(userLogin) : Пользователя с логином {userLogin} не существует"));
+        return user!;
+    }
+
+    public IEnumerable<Property> GetAllProperties()
+    {
+        var properties = new User().GetType().GetProperties();
+        return properties
+            .Select(property => new Property(property.Name, $"Тип свойства - {property.PropertyType}"))
+            .Where(property => property.Name != "login")
+            .Append(new Property("Пароль", "Пароль пользователя"));
+    }
+
+    public void UpdateUserProperties(IEnumerable<UserProperty> properties, string userLogin)
+    {
+        var user = _context.Users.FirstOrDefault(user => user.login == userLogin);
+        if (user is null) Error.Throw(_logger, new ArgumentException($"GetUserProperties(userLogin) : Пользователя с логином {userLogin} не существует"));
+            
+        foreach (var prop in properties)
+        {
+            var property = user!.GetType().GetProperties().FirstOrDefault(p => p.Name == prop.Name);
+            if (property is null) throw new ArgumentException("Такого свойства нет");
+            if(property.PropertyType == typeof(bool)) 
+                property.SetValue(user, prop.Value.Contains("true", StringComparison.OrdinalIgnoreCase));
+            else
+                property.SetValue(user, prop.Value);
+        }
+        _context.SaveChanges();
+    }
+
+    public IEnumerable<UserProperty> GetUserProperties(string userLogin)
+    {
+        var user = _context.Users.FirstOrDefault(user => user.login == userLogin);
+        if (user is null) Error.Throw(_logger, new ArgumentException($"GetUserProperties(userLogin) : Пользователя с логином {userLogin} не существует"));
+            
+        var properties = user!.GetType().GetProperties();
+        return properties
+            .Select(property => new UserProperty(property.Name, property.GetValue(user)?.ToString() ?? string.Empty))
+            .Where(property => property.Name != "login");
+    }
+
+    public bool IsUserExists(string userLogin)
+    {
+        return _context.Users.FirstOrDefault(user => user.login == userLogin) is not null;
+    }
+}
