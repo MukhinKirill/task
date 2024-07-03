f520f9c05977e0105df82020739bf0818befaae3
917sympathies <maraturazbakhtin@yandex.ru>
2024-07-03 17:54:40 +0400

    connector class implemented

--- Task.Connector/ConnectorDb.cs
+++ Task.Connector/ConnectorDb.cs
@@ -1,60 +1,83 @@
-﻿using Task.Integration.Data.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Task.Connector.Context;
+using Task.Connector.Entities;
+using Task.Connector.Errors;
+using Task.Connector.Interfaces;
+using Task.Connector.Services;
+using Task.Integration.Data.Models;
 using Task.Integration.Data.Models.Models;
+using Property = Task.Integration.Data.Models.Models.Property;
 
 namespace Task.Connector
 {
     public class ConnectorDb : IConnector
     {
+        public ILogger Logger { get; set; }
+        
+        private DatabaseContext? _context;
+        
+        private IUserInterface _userService;
+
+        private IPermissionService _permissionService;
+
+        public ConnectorDb()
+        {
+            
+        }
+        
         public void StartUp(string connectionString)
         {
-            throw new NotImplementedException();
+            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>().UseNpgsql(connectionString);
+            _context = new DatabaseContext(optionsBuilder.Options);
+            if(_context is null) Error.Throw(Logger, new NullReferenceException("База данных не инициализирована"));
+            
+            _userService = new UserService(_context!, Logger);
+            _permissionService = new PermissionService(_context!, Logger);
         }
-
+        
         public void CreateUser(UserToCreate user)
         {
-            throw new NotImplementedException();
+            _userService.CreateUser(user);
         }
 
         public IEnumerable<Property> GetAllProperties()
         {
-            throw new NotImplementedException();
+            return _userService.GetAllProperties();
         }
 
         public IEnumerable<UserProperty> GetUserProperties(string userLogin)
         {
-            throw new NotImplementedException();
+            return _userService.GetUserProperties(userLogin);
         }
 
         public bool IsUserExists(string userLogin)
         {
-            throw new NotImplementedException();
+            return _userService.IsUserExists(userLogin);
         }
 
         public void UpdateUserProperties(IEnumerable<UserProperty> properties, string userLogin)
         {
-            throw new NotImplementedException();
+            _userService.UpdateUserProperties(properties, userLogin);
         }
 
         public IEnumerable<Permission> GetAllPermissions()
         {
-            throw new NotImplementedException();
+            return _permissionService.GetAllPermissions();
         }
 
         public void AddUserPermissions(string userLogin, IEnumerable<string> rightIds)
         {
-            throw new NotImplementedException();
+            _permissionService.AddUserPermissions(userLogin, rightIds);
         }
 
         public void RemoveUserPermissions(string userLogin, IEnumerable<string> rightIds)
         {
-            throw new NotImplementedException();
+            _permissionService.RemoveUserPermissions(userLogin, rightIds);
         }
 
         public IEnumerable<string> GetUserPermissions(string userLogin)
         {
-            throw new NotImplementedException();
+            return _permissionService.GetPermissionByUserLogin(userLogin);
         }
-
-        public ILogger Logger { get; set; }
     }
 }
