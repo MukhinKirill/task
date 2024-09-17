3e130029c8fbef46f02397e21f938627e4df8d05
V0R0N <gfcnjjvvygc@gmail.com>
2024-09-17 21:31:17 +0300

    add parse connection string and remove test mssql

--- Task.Connector.Tests/UnitTest1.cs
+++ Task.Connector.Tests/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace Task.Connector.Tests
         static string postgreConnectionString = "";
         static Dictionary<string, string> connectorsCS = new Dictionary<string, string>
         {
-            { "MSSQL",$"ConnectionString='{mssqlConnectionString}';Provider='SqlServer.2019';SchemaName='AvanpostIntegrationTestTaskSchema';"},
+            { "MSSQL", $"ConnectionString='{mssqlConnectionString}';Provider='SqlServer.2019';SchemaName='AvanpostIntegrationTestTaskSchema';"},
             { "POSTGRE", $"ConnectionString='{postgreConnectionString}';Provider='PostgreSQL.9.5';SchemaName='AvanpostIntegrationTestTaskSchema';"}
         };
         static Dictionary<string, string> dataBasesCS = new Dictionary<string, string>
@@ -40,7 +40,7 @@ namespace Task.Connector.Tests
 
 
         [Theory]
-        [InlineData("MSSQL")]
+        // [InlineData("MSSQL")]
         [InlineData("POSTGRE")]
         public void CreateUser(string provider)
         {
@@ -52,7 +52,7 @@ namespace Task.Connector.Tests
         }
 
         [Theory]
-        [InlineData("MSSQL")]
+        // [InlineData("MSSQL")]
         [InlineData("POSTGRE")]
         public void GetAllProperties(string provider)
         {
@@ -63,7 +63,7 @@ namespace Task.Connector.Tests
         }
 
         [Theory]
-        [InlineData("MSSQL")]
+        // [InlineData("MSSQL")]
         [InlineData("POSTGRE")]
         public void GetUserProperties(string provider)
         {
@@ -76,7 +76,7 @@ namespace Task.Connector.Tests
         }
 
         [Theory]
-        [InlineData("MSSQL")]
+        // [InlineData("MSSQL")]
         [InlineData("POSTGRE")]
         public void IsUserExists(string provider)
         {
@@ -87,7 +87,7 @@ namespace Task.Connector.Tests
         }
 
         [Theory]
-        [InlineData("MSSQL")]
+        // [InlineData("MSSQL")]
         [InlineData("POSTGRE")]
         public void UpdateUserProperties(string provider)
         {
@@ -104,7 +104,7 @@ namespace Task.Connector.Tests
         }
 
         [Theory]
-        [InlineData("MSSQL")]
+        // [InlineData("MSSQL")]
         [InlineData("POSTGRE")]
         public void GetAllPermissions(string provider)
         {
@@ -116,7 +116,7 @@ namespace Task.Connector.Tests
         }
 
         [Theory]
-        [InlineData("MSSQL")]
+        // [InlineData("MSSQL")]
         [InlineData("POSTGRE")]
         public void AddUserPermissions(string provider)
         {
@@ -131,7 +131,7 @@ namespace Task.Connector.Tests
         }
 
         [Theory]
-        [InlineData("MSSQL")]
+        // [InlineData("MSSQL")]
         [InlineData("POSTGRE")]
         public void RemoveUserPermissions(string provider)
         {
@@ -145,7 +145,7 @@ namespace Task.Connector.Tests
             Assert.False(dataSetter.MasterUserHasRequestRight(dataSetter.GetRequestRightId(DefaultData.RequestRights[DefaultData.MasterUserRequestRights.First()].Name).ToString()));
         }
         [Theory]
-        [InlineData("MSSQL")]
+        // [InlineData("MSSQL")]
         [InlineData("POSTGRE")]
         public void GetUserPermissions(string provider)
         {
--- Task.Connector/ConnectorDb.cs
+++ Task.Connector/ConnectorDb.cs
@@ -1,60 +1,85 @@
-﻿using Task.Integration.Data.Models;
+﻿using System.Text.RegularExpressions;
+using Task.Integration.Data.Models;
 using Task.Integration.Data.Models.Models;
 
-namespace Task.Connector
+namespace Task.Connector;
+
+public class ConnectorDb : IConnector
 {
-    public class ConnectorDb : IConnector
+	public ILogger Logger { get; set; }
+
+	public void StartUp(string connectionString)
     {
-        public void StartUp(string connectionString)
-        {
-            throw new NotImplementedException();
-        }
+        var settings = ParseConnectionString(connectionString);
 
-        public void CreateUser(UserToCreate user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Property> GetAllProperties()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<UserProperty> GetUserProperties(string userLogin)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool IsUserExists(string userLogin)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateUserProperties(IEnumerable<UserProperty> properties, string userLogin)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Permission> GetAllPermissions()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void AddUserPermissions(string userLogin, IEnumerable<string> rightIds)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveUserPermissions(string userLogin, IEnumerable<string> rightIds)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<string> GetUserPermissions(string userLogin)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ILogger Logger { get; set; }
+        var connectionStr = settings["ConnectionString"];
     }
+
+    public void CreateUser(UserToCreate user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Property> GetAllProperties()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<UserProperty> GetUserProperties(string userLogin)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsUserExists(string userLogin)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateUserProperties(IEnumerable<UserProperty> properties, string userLogin)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Permission> GetAllPermissions()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddUserPermissions(string userLogin, IEnumerable<string> rightIds)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveUserPermissions(string userLogin, IEnumerable<string> rightIds)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<string> GetUserPermissions(string userLogin)
+    {
+        throw new NotImplementedException();
+    }
+
+
+
+	private static Dictionary<string, string> ParseConnectionString(string connectionString)
+	{
+		var result = new Dictionary<string, string>();
+
+		// (\w+) - >= 1 буквенно-цифровых символов (ключ)
+		// '([^']+)' - >= 1 символов в одинарных кавычках кроме самих кавычек (значение)
+		var pattern = @"(\w+)='([^']+)'";
+
+		var matches = Regex.Matches(connectionString, pattern);
+
+		foreach (Match match in matches)
+		{
+			var key = match.Groups[1].Value;
+			var value = match.Groups[2].Value;
+
+			result[key] = value;
+		}
+
+		return result;
+	}
 }
