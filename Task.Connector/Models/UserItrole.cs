579ac20e1ddac848dde04d443317719f03cd770c
MaxxImAAA <136116047+MaxxImAAA@users.noreply.github.com>
2024-10-08 19:31:49 +0300

    Add files via upload

---
+++ Task.Connector/Models/UserItrole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task.Connector.Models;
+
+public partial class UserItrole
+{
+    public string UserId { get; set; } = null!;
+
+    public int RoleId { get; set; }
+}
---
+++ Task.Connector/Models/UserRequestRight.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task.Connector.Models;
+
+public partial class UserRequestRight
+{
+    public string UserId { get; set; } = null!;
+
+    public int RightId { get; set; }
+}
