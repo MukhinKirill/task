bf51b7c9fd521b24852fa7d6f0442128e432521c
MaxxImAAA <136116047+MaxxImAAA@users.noreply.github.com>
2024-10-08 19:33:49 +0300

    Add files via upload

---
+++ Task.Connector/Exceptions/CustomException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Connector.Exceptions
+{
+    internal class CustomException : Exception
+    {
+        public CustomException(string message) : base(message)
+        {
+            
+        }
+    }
+}
