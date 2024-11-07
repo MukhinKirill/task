9dc93ecad59e5a4defbff156bc60791d28350b52
Blast <kingprostyle@yandex.ru>
2024-11-07 18:35:07 +0300

    initial db setup and scaffold

---
+++ Task.Connector/Models/Password.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task.Connector.Models;
+
+public partial class Password
+{
+    public int Id { get; set; }
+
+    public string UserId { get; set; } = null!;
+
+    public string Password1 { get; set; } = null!;
+}
---
+++ Task.Connector/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task.Connector.Models;
+
+public partial class User
+{
+    public string Login { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string MiddleName { get; set; } = null!;
+
+    public string TelephoneNumber { get; set; } = null!;
+
+    public bool IsLead { get; set; }
+}
