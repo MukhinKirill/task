315f432d8e7192b48f18d85ae3422022fbc3cc16
Сороко Михаил <darkserj18@gmail.com>
2024-11-15 18:07:15 +0300

    Применение db-first для создания моделей

---
+++ Task.Connector/Models/MigrationHistory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task.Connector.Models;
+
+public partial class MigrationHistory
+{
+    public string MigrationId { get; set; } = null!;
+
+    public string ProductVersion { get; set; } = null!;
+}
